7d0ef0d8bbfc73a96aa3bbfdcbd2f36f0146809b
Nicole Barkley <barkleylikecharles@icloud.com>
2022-05-17 10:46:20 -0400

    added associations

--- Develop/models/index.js
+++ Develop/models/index.js
@@ -4,6 +4,36 @@ const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
 
+//Create associations
+
+Product.belongsTo(Category, {
+  foreignKey: "category_id",
+  onDelete: "CASCADE",
+});
+
+Product.belongsToMany(Tag, {
+through: ProductTag,
+foreignKey: 'product_id'
+});
+
+Category.hasMany(Product, {
+  foreignKey: 'category_id',
+});
+
+Tag.belongsToMany(Product, { 
+  through: ProductTag, 
+  foreignKey: "tag_id",
+ });
+
+
+// Post.belongsToMany(User, {
+// through: Vote,
+// as: 'voted_posts',
+// foreignKey: 'post_id'
+// });
+// Vote.belongsTo(User, {
+// foreignKey: 'user_id'
+// });
 // Products belongsTo Category
 
 // Categories have many Products
